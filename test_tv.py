35e49d5cb90a0f477b1c12d778a46eedce1c74f2
diane <villalino.136534100519@depedqc.ph>
2024-05-24 12:01:59 +0800

    Turn on tv2 and set channel and volume

--- test_tv.py
+++ test_tv.py
@@ -13,8 +13,12 @@ def test_tv():
     tv1.turnOn()
     tv1.setChannel(30)
     tv1.setVolume(3)
-    
+
 # Turn on tv2 and set its channel to 3 and volume to 2
+    tv2.turnOn()
+    tv2.setChannel(3)
+    tv2.setVolume(2)
+    
 # ANSI escape codes for purple and bold
 # Print the states of tv1 and tv2 in purple and bold
 # Run the test
