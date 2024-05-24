94f27d219ae67426872435664e47ba576b98dec8
diane <villalino.136534100519@depedqc.ph>
2024-05-24 10:30:54 +0800

    Initialize the volume level to 1

--- tv.py
+++ tv.py
@@ -3,6 +3,7 @@ class TV:
     # Constructor
     def __init__(self):
         self.channel = 1
+        self.volumelevel = 1
 # Method to turn on the TV
 # Method to turn off the TV
 # Method that sets the current channel
