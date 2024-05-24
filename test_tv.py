5ac772355a595593f25f5e5504bbbfa67c4c9124
diane <villalino.136534100519@depedqc.ph>
2024-05-24 11:45:49 +0800

    Pseudocode

---
+++ test_tv.py
@@ -0,0 +1,9 @@
+# Test drive
+# Import the TV class
+# Define TestTV
+# Create two objects
+# Turn on tv1 and set its channel to 30 and volume to 3
+# Turn on tv2 and set its channel to 3 and volume to 2
+# ANSI escape codes for purple and bold
+# Print the states of tv1 and tv2 in purple and bold
+# Run the test
