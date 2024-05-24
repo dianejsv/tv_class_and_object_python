e8aa09be395cec2d0007c520e635180ac8bc09c8
diane <villalino.136534100519@depedqc.ph>
2024-05-24 10:23:41 +0800

    Constuctor

--- tv.py
+++ tv.py
@@ -1,6 +1,7 @@
 # Class representing the Television
 class TV:
     # Constructor
+    def __init__(self):
 # Method to turn on the TV
 # Method to turn off the TV
 # Method that sets the current channel
